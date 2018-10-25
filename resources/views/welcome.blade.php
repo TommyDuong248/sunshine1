47a51fdf6643d316da45dfeb52e381c81e4e7224
TommyDuong <dqtungc17102@cusc.ctu.eud.vn>
2018-10-25 18:57:59 +0700

    Update Tile index page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Duong Quang Tung
                 </div>
 
                 <div class="links">
