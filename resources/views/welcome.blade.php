b2223b75fac55968cec4d9183b3dd5a53d3a2da1
TommyDuong <dqtungc17102@cusc.ctu.eud.vn>
2018-10-25 19:03:08 +0700

    capnhat

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Duong Quang Tung
+                    "Duong Quang Tung"
                 </div>
 
                 <div class="links">
